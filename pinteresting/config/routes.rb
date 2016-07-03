27b77cc3bb7cc6041d2e555073c430d9ba339fe7
Todd Wozny <toddwozny@gmail.com>
2016-07-03 13:31:38 -0400

    Creating home and about pages

--- pinteresting/config/routes.rb
+++ pinteresting/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get "about" => "pages#about"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
