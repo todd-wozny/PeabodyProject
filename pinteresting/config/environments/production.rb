2ae8630b66ca0fd03f2edb7946363f446553c259
Todd Wozny <toddwozny@gmail.com>
2016-09-05 17:19:07 -0400

    Devise

--- pinteresting/config/environments/production.rb
+++ pinteresting/config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Set this to actual HostName mshoward-classroom
+  config.action_mailer.default_url_options = { host: 'mshoward-classroom' }
 end
